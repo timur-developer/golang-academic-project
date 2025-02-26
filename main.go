255fd0f5c89fe045caeb814039dc97e8ec169522
Timur Gilyazov <progwayoftimur@gmail.com>
2025-02-26 17:38:39 +0300

    Create local repository

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
