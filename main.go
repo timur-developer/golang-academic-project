e0ef49c605db139e38cd759d13bd115e45f5bca4
Timur Gilyazov <progwayoftimur@gmail.com>
2025-02-26 18:11:06 +0300

    Added Post Handler (change global var task), edited Get Handler (return Hello, 'task')

--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+var task string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello,", task)
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	req := requestBody{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		fmt.Fprintln(w, "Error decoding request")
+	}
+	if req.Message != "" {
+		task = req.Message
+		fmt.Fprintln(w, "Task was updated successfully")
+	} else {
+		fmt.Fprintln(w, "Task-message could not be empty")
+	}
+
 }
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/", GetHandler).Methods("GET")
+	router.HandleFunc("/", PostHandler).Methods("POST")
 
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	http.ListenAndServe(":8080", router)
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal("Could not run server")
+	}
 }
